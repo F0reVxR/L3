c80f5da4a654a811bd0ee45e381056605a55f54a
F0reVxR <f1n2l0v3_o6e9@tutamail.com>
2025-09-29 15:28:02 +0300

    1

--- l3_main.py
+++ l3_main.py
@@ -1,4 +1,4 @@
 Name = input('Ваше имя - ')
-Age = int(input('Ваш возраст - '))
+Age = int(input('Ваш возраст в годах- '))
 Years_left = float(63 - Age)
 print(f"До пенсионного возраста вам осталось {Years_left} лет")
